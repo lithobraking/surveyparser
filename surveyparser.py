1cff7291e97ca354b9c20d959fd9d79630a6a12a
Parker <panoblesse@gmail.com>
2026-04-22 14:13:38 -0700

    prepare_data adjusts based on key's value type

--- surveyparser.py
+++ surveyparser.py
@@ -12,22 +12,32 @@ fn = askopenfilename()
 responses = pd.read_csv(fn)
 responses = responses.drop('Timestamp', axis=1)
 
+print("Ingested response data. This is the current table:")
+print(responses)
+
+print("Filtering irrelevant data form table...")
 def prepare_data(table):
     working_data = table.copy()
     filter_questions = {
         "consent": "I Do Not Agree", 
-        "What is your age group?": "55 - 64", 
+        "What is your age group?": ["35 - 44", "55 - 64", "65 or older"], 
         "What is your current relationship status?": "Single"
         }
     
     for key, value in filter_questions.items():
-        print(type(value))
-        working_data = working_data[working_data[key].str.contains(value) == False]
+        if (isinstance(value, str)):
+            working_data = working_data[working_data[key].str.contains(value) == False]
+        if (isinstance(value, list)):
+            i = 0
+            while i < len(value):
+                if (value[i] in value):
+                    working_data = working_data[working_data[key].str.contains(value[i]) == False]
+                i += 1
 
     working_data = working_data.reset_index(drop=True)
     return working_data
 
 responses = prepare_data(responses)
-print("Removed irrelevant data. New table looks like this:")
+print("Done! New table looks like this:")
 print(responses)
 
