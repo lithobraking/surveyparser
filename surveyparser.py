71c0e746c209f475605f6b4780b84f152aee2f2a
Parker <panoblesse@gmail.com>
2026-04-22 13:41:22 -0700

    encapsulates data cleanup inside a function

--- surveyparser.py
+++ surveyparser.py
@@ -12,7 +12,22 @@ fn = askopenfilename()
 responses = pd.read_csv(fn)
 responses = responses.drop('Timestamp', axis=1)
 
-responses = responses[responses["consent"].str.contains("I Do Not Agree") == False]
-responses = responses.reset_index(drop=True)
+def prepare_data(table):
+    working_data = table.copy()
+    filter_questions = {
+        "consent": "I Do Not Agree", 
+        "What is your age group?": "55 - 64", 
+        "What is your current relationship status?": "Single"
+        }
+    
+    for key, value in filter_questions.items():
+        print(type(value))
+        working_data = working_data[working_data[key].str.contains(value) == False]
+
+    working_data = working_data.reset_index(drop=True)
+    return working_data
+
+responses = prepare_data(responses)
+print("Removed irrelevant data. New table looks like this:")
 print(responses)
 
