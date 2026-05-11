a9542ef6742dc6d79c724e4d7220267115d22cf2
Parker <panoblesse@gmail.com>
2026-05-10 20:10:38 -0700

    evaluates overall relationship quality based on score total

--- RelationshipQuality.py
+++ RelationshipQuality.py
@@ -7,7 +7,7 @@ def get_relationship_quality_score(data, idx):
         total += data.iat[idx, (items[i] + 4)]
         i += 1
 
-    return total
+    return total - get_love_score
 
 def get_satisfaction_score(data, idx):
     total = 0
@@ -43,6 +43,10 @@ def get_trust_score(data, idx):
     return total / len(items)
 
 def get_love_score(data, idx):
+    # i messed up my survey design so this data is rendered kind of 
+    # useless because it's missing one datapoint and i can't make any
+    # meaningful calculations with it, so now this function just serves
+    # to help remove this data from the score total :(
     total = 0
     i = 0
     items = [33, 40]
@@ -60,6 +64,18 @@ def evaluate_subscore(subscore):
     elif (subscore >= 3 and subscore <= 5):
         result = "MODERATE"
     else:
+        result = "HIGH"
+    
+    return result
+
+def get_overall_satisfaction():
+    result = "blank"
+    relationship_quality = get_relationship_quality_score
+    if (relationship_quality >= 3 and relationship_quality < 10):
+        result = "LOW"
+    elif (relationship_quality >=10 and relationship_quality <= 17):
+        result = "MODERATE"
+    elif (relationship_quality >= 17 and relationship_quality <= 24):
         result = "HIGH"
     
     return result
